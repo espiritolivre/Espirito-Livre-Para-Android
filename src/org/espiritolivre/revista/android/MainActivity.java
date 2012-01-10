2bbc4a34fcf378f0ed81afffde4b12f539ddfd01
Rodrigo Carvalho <rcsilva83@gmail.com>
2012-01-10 16:56:11 -0200

    Alterado para utilizar os feeds do site da Revista Espírito Livre.

--- src/org/espiritolivre/revista/android/MainActivity.java
+++ src/org/espiritolivre/revista/android/MainActivity.java
@@ -6,57 +6,43 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends Activity implements OnClickListener {
+public class MainActivity extends Activity {
+	
     private static final int REQUEST_CODE = 10;
+    
+    private static final String[] LABELS_CATEGORIAS = new String[]
+    		{"Edições", "Notícias"};
+
+    private static final String[] FEEDS_CATEGORIAS = new String[]
+    		{"http://www.revista.espiritolivre.org/category/lancamentos/feed", "http://www.revista.espiritolivre.org/category/noticias/feed"};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        // Aqui nos declaramos todos os botoes da interface que vao ter
-        // uma acao associada a eles
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, LABELS_CATEGORIAS);
+        
+        ListView categorias = (ListView) findViewById(R.id.categorias);
+        categorias.setAdapter(adapter);
+        categorias.setClickable(true);
+        categorias.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-        // Botao ultimos artigos
-        Button thebtnLatestArticles = (Button) findViewById(R.id.btnLatestArticles);
-        thebtnLatestArticles.setOnClickListener(this);
-
-        // Botao ultimos comentarios
-        Button thebtnLatestComments = (Button) findViewById(R.id.btnLatestComments);
-        thebtnLatestComments.setOnClickListener(this);
+			public void onItemClick(AdapterView<?> adapter, View view, int posicao, long id) {
+				exibirFeed(LABELS_CATEGORIAS[posicao], FEEDS_CATEGORIAS[posicao]);
+			}
+		});
     }
 
-    // E aqui declaramos as acoes num loop switch:
-
-    // Atencao se voce quiser modificar: os dois primeiros cases abaixo
-    // funcionam de forma muito similar: eles chamam a tela da activity
-    // FeedReader e passam a ela dois parametros: feedTitle e feedURL.
-    // O primeiro e o nome do feed, por exemplo "ultimos artigos" e o
-    // segundo e a sua URL. Se quiser adaptar para os seu site, apenas
-    // modifique essas informacoes com os dados corretos! Nao mexa no resto!
-    // Se quiser adicionar mais botoes, declare-os no main.xml, siga o
-    // exemplo acima de declaracao de Button e adicione um novo case abaixo.
-
-    public void startTheActivity(String theTitle, String theURL) {
+    public void exibirFeed(String theTitle, String theURL) {
         Intent i = new Intent(MainActivity.this, FeedReaderActivity.class);
         i.putExtra("feedTitle", theTitle);
         i.putExtra("feedURL", theURL);
         startActivityForResult(i, REQUEST_CODE);
-    }
-
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnLatestArticles: // Botao ultimos artigos
-                startTheActivity("Últimos artigos", "http://espacoliberdade.blog.br/blog/feed/");
-                break;
-            case R.id.btnLatestComments: // Botao ultimos comentarios
-                startTheActivity("Últimos comentários", "http://espacoliberdade.blog.br/blog/comments/feed/");
-                break;
-        }
-
     }
     
     @Override
